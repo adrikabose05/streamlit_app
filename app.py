0e7406cf56b7dd49977ca4091780a35600f0f1ce
adrikabose05 <adrikabose05@gmail.com>
2025-06-17 22:12:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import pandas as pd
+st.set_page_config(
+  page_title="Covid 19",
+  layout="wide"
+  initial_sidebar_state="expanded"
+)
 
-st.title("Hello Streamlit!")
-st.write("This is your first Streamlit app.")
+st.title("📁 Load Local Dataset")
+
+df = pd.read_csv("data.csv")
+st.write("Loaded Data:")
+st.dataframe(df)
+
+st.title("Covid 19 Dashboard")
+st.write("")
